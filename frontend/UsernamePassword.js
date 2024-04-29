81d1b135fe7d5ba8bd5ad4865d9430be435f0474
anwithayi <azhao3@seattleu.edu>
2024-04-29 01:59:30 -0700

    more button added and communication good

---
+++ frontend/UsernamePassword.js
@@ -0,0 +1,48 @@
+const usernameInput = document.getElementById('username-input');
+const StartGameButton = document.getElementById('start-game-button');
+const test = document.getElementById('test');
+
+
+test.onclick = function() {
+  websocket.send("test");
+  feedback.textContent='tested';
+}
+StartGameButton.onclick = function() {
+  const username = usernameInput.value;
+  websocket.send("checkUsername "+username);
+}
+
+function checkUsername(message_passed_from_the_server){
+  if (message_passed_from_the_server === "Username is taken.") {
+    feedback.textContent ="Username is already taken. Please try again.";
+  }else if(message_passed_from_the_server === "username can't have space."){
+    feedback.textContent ="Username can't have space.";
+} else if (message_passed_from_the_server === "Username OK") {
+  feedback.textContent ="plyer added.";
+    contentChange();
+} else {
+    // Handle unexpected server response
+    console.log("Unexpected server response: " + message_passed_from_the_server);
+}
+}
+
+function contentChange(){
+  const contentChange = document.getElementById('content');
+  contentChange.innerHTML=`
+  <div id="game-container">
+  <button id="quit-button">quit</button>
+<button id="check-button">check your score</button>
+<button id="rule-button">game rule</button>
+<br><br><br><br><br><br>
+<div id="status">Waiting for players...</div>
+<br><br><br><br><br><br>
+<input type="number" id="guess-input" placeholder="Enter your guess(100 -999)" min="100" max="999">
+<button id="guess-button">GUESS!</button>
+<button id="pass-button">PASS.</button>
+<button id="test-button">test.</button>
+<div id="feedback">feedback</div>
+</div>
+<script src="guessNumber.js"></script>
+`;
+}
+
